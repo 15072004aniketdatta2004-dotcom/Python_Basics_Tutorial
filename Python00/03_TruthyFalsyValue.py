377e4c56aa56c85f91c593d61a475013d21cd222
15072004aniketdatta2004-dotcom <15072004aniketdatta2004@gmail.com>
2026-08-10 14:20:29 +0000

    TruthyFalsyValues

--- Python00/03_TruthyFalsyValue.py
+++ Python00/03_TruthyFalsyValue.py
@@ -6,11 +6,32 @@ class TruthyFalsyValue:
         return int(bool(self.num))
     def power(self, exponent: float,exponent2: float)-> float:
         return self.num ** exponent ** exponent2
-    d
+    
 
 if __name__ == "__main__":
     num:float = float(input("Enter a value to check if it's truthy or falsy: "))
     truthy_falsy = TruthyFalsyValue(num)
+    a:int = 4
+    b:int = 5
+    c:int = 6
+    d:bool = True
+    e:bool = False
+    bool1: bool = (d+d) >= 2 and (not e)
+    bool2: bool = (not e) and (6*d == 12/2)
+    bool3: bool = (d or (e)) and (a > b)
+    bool4: bool = d and (a>b)
+    bool5: bool = (not d) or (b!=c)
+    bool6: bool =  (d and ( not e )) or (a>b)
+    bool7: bool = (a%b==2) and ((not d) or e)
+    bool8: bool = (a%b==True) and ((not d) or e)
+    print(bool1)
+    print(bool2)
+    print(bool3)
+    print(bool4)
+    print(bool5)
+    print(bool6)
+    print(bool7)
+    print(bool8)
     print(f"The output for {num} is: {truthy_falsy.is_truthy()}")
     exponent: float = float(input("Enter an exponent to calculate the power: "))
     exponent2: float = float(input("Enter another exponent to calculate the power: "))
---
+++ Python00/06_Ifelifelse.py
@@ -0,0 +1,37 @@
+class IfelifElse:
+    def __init__(self, number1, number2):
+        self.number1 = number1
+        self.number2 = number2
+    def compare_numbers(self):
+        if self.number2 > self.number1:
+            print(f"{self.number2} is greater than {self.number1}")
+        elif self.number2 < self.number1:
+            print(f"{self.number2} is less than {self.number1}")
+        elif self.number2 == self.number1:
+            print(f"{self.number2} is equal to {self.number1}")
+        else:
+            print("Invalid input. Please provide valid numbers.")
+        print("Hello There! This is a simple comparison program.")
+    def controlFlow(self):
+        x = int(input("Enter a number: "))
+        print("You entered:", x)
+        if x < 10:
+            print("The number is less than 10.")
+        elif x <= 99:
+            print("The number is between 10 and 99.")
+        # """ else:
+        #        if x <= 99:
+        #           print("The number is between 10 and 99.")
+        #        else:
+        #           print("The number is 100 or greater.")
+        # """
+        else:
+            print("The number is 100 or greater.")
+        print("This is a simple control flow demonstration.")    
+
+if __name__ == "__main__":
+    number1 = float(input("Enter the first number: "))
+    number2 = float(input("Enter the second number: "))
+    comparison = IfelifElse(number1, number2)
+    comparison.compare_numbers()
+    comparison.controlFlow()
