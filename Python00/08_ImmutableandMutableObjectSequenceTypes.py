ccd5169fb58cb571fef26676cfe3b495135215af
Aniket Datta <15.07.2004aniketdatta02@gmail.com>
2026-08-18 02:00:14 +0530

    Starting with SequenceTypes

---
+++ Python00/08_ImmutableandMutableObjectSequenceTypes.py
@@ -0,0 +1,10 @@
+class ImmutableAndMutableObjectSequenceTypes:
+    def __init__(self):
+        pass
+
+    def ImmutableObjectSequenceTypes(self):
+        pass
+
+if "__name__" == "__main__":
+    im = ImmutableAndMutableObjectSequenceTypes()
+    im.ImmutableObjectSequenceTypes()
