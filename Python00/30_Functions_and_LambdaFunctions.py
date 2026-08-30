cb83ce2e8507f92d80dcc494ad10bcfd1cd10027
Aniket Datta <15.07.2004aniketdatta02@gmail.com>
2026-08-30 23:31:42 +0530

    FunctionsImportant

--- Python00/30_Functions_and_LambdaFunctions.py
+++ Python00/30_Functions_and_LambdaFunctions.py
@@ -1,3 +1,8 @@
+import sys
+import typing
+import ctypes
+# import nanoleafapi
+import os
 class Functions_and_LambdaFunctions:
     # A constructor is also a method to create objects which is called in the main function 
     # as Functions_and_LambdaFunctions() for non parameterized constructor and 
@@ -13,16 +18,93 @@ class Functions_and_LambdaFunctions:
         if name:
             return f"Happy Birthday to {name}!"
         return "Happy Birthday to You!"
-
-    def main(self)->None:
-      if not self.name:  
-        print(self.happyBirthday()) # the function is called
-      else:  
-        print(self.happyBirthday(self.name)) # the function with a parameter is called
+    def say_hello(self,name:str=None)->None:
+       if name:
+        print(f"Hello {name}!")
+       else: 
+        print("Hello")
+    def averageOfTwoNumbers(self, num1:float, num2:float)->float:
+        # if num1 is None:
+        #      print("Enter the Value of num1")
+        # if num2 is None:
+        #     print("Enter the Value of num2")
+        sum=num1+num2
+        return (sum/2.0) # Return the value of the function
+    # declaration     
+    def cube(self,num:float)->float:
+        # signature of the function is cube(self,num:float)->float, 
+        # where self is the instance of the class, num is the parameter of the function, 
+        # and float is the return type of the function.
+        # Declaration of the function
+        # Definition of the function
+      return num**3 # Return the value of the function
+    def passFunction(self)->None:
+        pass
+    def processNumber(self, x:float):
+        print(id(x)) # object id = 2370777963824
+        x=72.0
+        print(id(x)) # object id =2370775005072
+        return (x+3) # 75
+    def funcA(self,a,b):
+        print(id(a),id(b)) #Object id(a,b) = 140724970292696 1339019943216
+        print(a,b)
+    def mySum(self,x,y):
+        sum=x+y
+        return sum
+    def  funcB(self,a,b,c)->None:
+        print(a,b,c)
+    def my_food(self,food:list[str])->None:
+        print(id(food)) # same object reference as fruits
+        for x in food:
+            print(x,end=" ") 
         
+    def main(self)->None:
+      num0:float= float(input("Enter the number whose cube has to be computed:"))
+      print(f"The cube of {num0} is: {self.cube(num0)}",end="\n") # function calling 
+      num1:float = float(input("Enter num1 value:"))
+      num2:float = float(input("Enter num2 value:"))
+      print(f"the return type of pass function is: {self.passFunction.__annotations__['return']}") # None  
+      y:float=54.0
+      print(id(y)) #object id = 2370777963824
+      res=self.processNumber(y) 
+      print(id(res)) # object id = 2370777965104
+      print(res)
+      y1:float=4.0 # object id=1339016783440
+      print(id(y1))
+      self.funcA(12,y)
+      self.funcA(y1,y1)
+      self.funcA(id(12),id(y))
+      self.funcA(id(y1),id(y1))
+      if not self.name:
+        self.say_hello() #Once the function is defined , we can call this
+        print(self.happyBirthday()) # the function is called
+        #self.callmeafunction() # AttributeError: 'Functions_and_LambdaFunctions' object has no attribute 'callmeafunction'
+        print(self.averageOfTwoNumbers(num1,num2))
+      else:
+        self.say_hello(self.name)  
+        print(self.happyBirthday(self.name)) # the function with a parameter is called
+        print(f"average function({num1},{num2})={self.averageOfTwoNumbers(num1,num2)}")
+        print(self.cube(2)+self.cube(3))#35 
+        a0:int=10
+        b0:int=20
+        print(id(a0),id(b0))
+        c=self.mySum(a0,b0)
+        print(id(a0),id(b0))
+        print(c)
+        print(sys._getframe().f_locals.keys())
+        #print(sys.getprofile())
+        print(sys._getframe().f_globals.keys())
+        print(sys._getframe().f_code.co_name)
+        x0:int = 1
+        y0:int = 2
+        z0:int = 3
+        print(self.funcB(x0,y0,z0))
+        fruits:list[str]=[input("Enter a fruit name:") for _ in range(3)]
+        print(id(fruits)) # same object reference as food arguement in fruits
+        print(self.my_food(fruits))
+
     # def (self,name=None)->None:
     #   print("No name!")
-      
 if __name__ == "__main__":
     name:str=input("Enter your name: ")
     obj = Functions_and_LambdaFunctions(name)
