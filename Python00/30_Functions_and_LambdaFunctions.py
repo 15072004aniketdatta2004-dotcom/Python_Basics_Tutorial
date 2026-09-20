fbdd09c564a2ef58d71c017c3aff53c6debc3d63
Aniket Datta <15.07.2004aniketdatta02@gmail.com>
2026-09-20 14:55:51 +0530

    Lambdafunctions

--- Python00/30_Functions_and_LambdaFunctions.py
+++ Python00/30_Functions_and_LambdaFunctions.py
@@ -421,4 +421,10 @@ if __name__ == "__main__":
     result = hofun(f,[1,3,2]) # Higher Order Functions
     print(result)
     f(seq=[1,2],i=0) #whenever you want to explicitly mention any arguement krrping an arguement in between set to its default value
-    
+    # Lambda functions
+    power = lambda x,y:x**y
+    print(power(2,3)) #8
+    #Factorial's Lambda function
+    factorial= lambda n:n*factorial(n-1) if n>0 else 1
+    print(factorial(5)) #120
+   
